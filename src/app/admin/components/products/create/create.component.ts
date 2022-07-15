e9419bb7e0b990af44d7d367f11dea3ff181ad96
Bkortun <bariskorun@gmail.com>
2022-07-15 19:09:36 +0300

    15.07.2022

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -26,6 +26,12 @@ export class CreateComponent implements OnInit {
         messageType: MessageType.Success,
         position: Position.TopRight
       });
+    },errorMessage=>{
+      this.alertify.message(errorMessage,{
+        dismissOthers:true,
+        messageType:MessageType.Error,
+        position:Position.TopRight
+      });
     });
   }
 
